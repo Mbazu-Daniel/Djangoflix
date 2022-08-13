8fe096864eb31b8e1bbf7962bddeb09f4491f2c6
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-08-13 14:33:07 +0100

    create unique constraint for videos, overwrite slug and test slug

--- Backend/videos/models.py
+++ Backend/videos/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
+
 # Create your models here.
 class Video(models.Model):
     class VideoStateOptions(models.TextChoices):
@@ -7,16 +9,15 @@ class Video(models.Model):
         PUBLISH = 'PU', "Publish"
         DRAFT = 'DR', "Draft"
 
-
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
-    video_id = models.CharField(max_length= 200)
-    active = models.BooleanField(default = True)
-    # timestamp
-    # updated 
-    state = models.CharField(max_length = 2, choices = VideoStateOptions.choices, default = VideoStateOptions.DRAFT)
-    publish_timestamp = models.DateTimeField(auto_now_add = False, auto_now = False, blank = True, null = True)
+    video_id = models.CharField(max_length=200, unique=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
     # Customize the name active to published on the admin panel
     @property
@@ -29,16 +30,25 @@ class Video(models.Model):
             self.publish_timestamp = timezone.now()
         elif self.state == self.VideoStateOptions.DRAFT:
             self.publish_timestamp = None
+
+        if self.slug is None:
+            self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
+
 """ This model is a proxy model to monitor our published videos"""
+
+
 class VideoAllProxy(Video):
     class Meta:
         proxy = True
         verbose_name = "All Video"
         verbose_name_plural = "All Videos"
 
+
 """ This model is a proxy model to monitor our published videos"""
+
+
 class VideoPublishedProxy(Video):
     class Meta:
         proxy = True
--- Backend/videos/tests.py
+++ Backend/videos/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.utils.text import slugify
 from .models import Video
 
 
@@ -7,8 +8,8 @@ from .models import Video
 
 class VideoModelTestCase(TestCase):
     def setUp(self):
-        Video.objects.create(title="This is my title")
-        Video.objects.create(title="This is my title2", state=Video.VideoStateOptions.PUBLISH)
+        self.obj_a = Video.objects.create(title="This is my title", video_id = "dafa")
+        self.obj_b = Video.objects.create(title="This is my title2", state=Video.VideoStateOptions.PUBLISH, video_id = "daf")
 
     def test_valid_title(self):
         title = "This is my title"
@@ -28,3 +29,9 @@ class VideoModelTestCase(TestCase):
         now = timezone.now()
         published_qs = Video.objects.filter(publish_timestamp__lte = now)
         self.assertTrue(published_qs.exists())
+
+    def test_slug_field(self):
+        title = self.obj_a.title
+        test_slug = slugify(title)
+        self.assertEqual(test_slug, self.obj_a.slug)
+        # to learn how to make a better slug, check out DJAANGO MODELS Unleashed
