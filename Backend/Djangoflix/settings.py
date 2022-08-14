8573b76cedfde1856aa03e324730eb424a6efc48
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-08-14 03:45:53 +0100

    created enVars to hide secret keys

--- Backend/Djangoflix/settings.py
+++ Backend/Djangoflix/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n2e8e*07kk=#9x(o8zk-78)bjox*nd8@fidxer*!63sw343x23"
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
